4993d09f164c7f38f338ec156719fc1cd2870aef
Ahmad Hijazi <ahmadhijazi_me@hotmail.com>
2024-03-26 12:45:25 +0100

    bug fixes

--- api/routes/models/user.js
+++ api/routes/models/user.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose');
 
-const orderSchema = mongoose.Schema({
+const mongoose = require('mongoose');
+const UserSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     email: {type: String, require: true, unique: true, match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/},
     password: {type: String, require: true}
     
 });
-
-module.exports = mongoose.model("User", orderSchema)
+module.exports = mongoose.model("User", UserSchema)
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const User = require('../models/user')
+const User = require('./models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 
 router.post('/signup',(req,res,next)=>{
@@ -35,7 +37,7 @@ User.find({
                     user.save()
                     .then(result =>{
                         res.status(201).json({
-                            message: "User created!"
+                            message: "User created!",
                         })
                     })
                     .catch(err =>{
@@ -53,6 +55,62 @@ User.find({
 })
 .catch()
 
+})
+
+router.post('/login',(req,res,next)=>{
+
+    User.find({
+        email: req.body.email,
+    })
+    .exec()
+    .then(user =>{
+        if(user.length < 1){
+            res.status(401).json({
+                message: "Auth failed (user exists)"     
+            })
+        }
+        else {
+            bcrypt.compare(req.body.password, user[0].password,async (err, response)=>{
+
+                if(err){
+                    res.status(401).json({
+                        message: "Auth failed"
+                    })
+                }
+                if(response) {
+
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id,
+                    },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: "1h"
+                        });
+
+                    res.status(200).json({
+                        message: 'Auth successful',
+                        token: token
+                    })
+      
+                }else {
+                    res.status(401).json({
+                        message: "Auth failed"
+                    })
+                }
+            
+            })
+    
+    
+
+        }
+    })
+    .catch(err =>{
+        res.status(500).json({
+            error: err
+        })
+    })
+    
 
 
 })
